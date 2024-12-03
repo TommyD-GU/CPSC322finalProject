f640f8e7e3ab1261bffb92ad058155a60b4aac72
Malia Recker <maliarecker@gmail.com>
2024-12-03 07:23:47 +0000

    11mon

--- myknnclassifier.py
+++ myknnclassifier.py
@@ -17,26 +17,26 @@
 """
 import utils
 class MyKNeighborsClassifier:
-    """Represents a simple k nearest neighbors classifier.
+    """Represents a simple k-nearest neighbors classifier.
 
     Attributes:
-        n_neighbors(int): number of k neighbors
-        X_train(list of list of numeric vals): The list of training instances (samples).
-                The shape of X_train is (n_train_samples, n_features)
-        y_train(list of obj): The target y values (parallel to X_train).
-            The shape of y_train is n_samples
+        n_neighbors (int): Number of k neighbors.
+        X_train (list of list of numeric vals): The list of training instances (samples).
+            The shape of X_train is (n_train_samples, n_features).
+        y_train (list of obj): The target y values (parallel to X_train).
+            The shape of y_train is n_samples.
 
     Notes:
         Loosely based on sklearn's KNeighborsClassifier:
-            https://scikit-learn.org/stable/modules/generated/sklearn.neighbors.KNeighborsClassifier.html
-        Terminology: instance = sample = row and attribute = feature = column
+        https://scikit-learn.org/stable/modules/generated/sklearn.neighbors.KNeighborsClassifier.html
+        Terminology: instance = sample = row and attribute = feature = column.
         Assumes data has been properly normalized before use.
     """
     def __init__(self, n_neighbors=3):
         """Initializer for MyKNeighborsClassifier.
 
         Args:
-            n_neighbors(int): number of k neighbors
+            n_neighbors (int): Number of k neighbors.
         """
         self.n_neighbors = n_neighbors
         self.X_train = None
@@ -46,163 +46,60 @@ class MyKNeighborsClassifier:
         """Fits a kNN classifier to X_train and y_train.
 
         Args:
-            X_train(list of list of numeric vals): The list of training instances (samples).
-                The shape of X_train is (n_train_samples, n_features)
-            y_train(list of obj): The target y values (parallel to X_train)
-                The shape of y_train is n_train_samples
+            X_train (list of list of numeric vals): The list of training instances (samples).
+                The shape of X_train is (n_train_samples, n_features).
+            y_train (list of obj): The target y values (parallel to X_train).
+                The shape of y_train is n_train_samples.
 
         Notes:
-            Since kNN is a lazy learning algorithm, this method just stores X_train and y_train
+            Since kNN is a lazy learning algorithm, this method just stores X_train and y_train.
         """
         self.X_train = X_train
         self.y_train = y_train
 
     def kneighbors(self, X_test):
-        """Determines the k closes neighbors of each test instance.
+        """Determines the k closest neighbors of each test instance.
 
         Args:
-            X_test(list of list of numeric vals): The list of testing samples
-                The shape of X_test is (n_test_samples, n_features)
+            X_test (list of list of numeric vals): The list of testing samples.
+                The shape of X_test is (n_test_samples, n_features).
 
         Returns:
-            distances(list of list of float): 2D list of k nearest neighbor distances
-                for each instance in X_test
-            neighbor_indices(list of list of int): 2D list of k nearest neighbor
-                indices in X_train (parallel to distances)
+            distances (list of list of float): 2D list of k nearest neighbor distances
+                for each instance in X_test.
+            neighbor_indices (list of list of int): 2D list of k nearest neighbor
+                indices in X_train (parallel to distances).
         """
-
-        all_distances = []
-        all_neighbors = []
-        
+        distances = []
+        neighbor_indices = []
         for test_instance in X_test:
             dist = []
-            
-            # do euclidean distance
+            # Compute Euclidean distance
             for i, train_instance in enumerate(self.X_train):
-                distance = utils.euclid_dist(test_instance, train_instance)
-                dist.append((distance, i))
-                
-            # sort the dist
-            for j in range(len(dist)):
-                for k in range(j + 1, len(dist)):
-                    if dist[j][0] > dist[k][0]:
-                        dist[j], dist[k] = dist[k], dist[j]
-            
-            # take the k smallest distances and their indices
-            current_distances = [d[0] for d in dist[:self.n_neighbors]]
-            current_neighbors = [d[1] for d in dist[:self.n_neighbors]]
-            
-            all_distances.append(current_distances)
-            all_neighbors.append(current_neighbors)
-        
-        return all_distances, all_neighbors
-                
+                distance = utils.euclid_dist(test_instance, train_instance)  # Calculate distance
+                dist.append((distance, i))  # Store distance and index
+            dist.sort(key=lambda x: x[0])  # Sort by distance
+            # Extract the k smallest distances and their corresponding indices
+            k_distances = [d[0] for d in dist[:self.n_neighbors]]
+            k_indices = [d[1] for d in dist[:self.n_neighbors]]
+            distances.append(k_distances)
+            neighbor_indices.append(k_indices)
+        return distances, neighbor_indices
 
     def predict(self, X_test):
         """Makes predictions for test instances in X_test.
 
         Args:
-            X_test(list of list of numeric vals): The list of testing samples
-                The shape of X_test is (n_test_samples, n_features)
+            X_test (list of list of numeric vals): The list of testing samples.
+                The shape of X_test is (n_test_samples, n_features).
 
         Returns:
-            y_predicted(list of obj): The predicted target y values (parallel to X_test)
+            y_predicted (list of obj): The predicted target y values (parallel to X_test).
         """
-
-        # get the nearest neighbors using kneighbors
-        _, neighbor_indices = self.kneighbors(X_test)
-
+        _, neighbor_indices = self.kneighbors(X_test)  # Get neighbor indices
         y_predicted = []
-
-        # for each test instance, get the labels of its nearest neighbors
-        for neighbors in neighbor_indices:
-            # get the corresponding labels from y_train
-            neighbor_labels = [self.y_train[i] for i in neighbors]
-
-            # use the most frequent label of the neighbors
-            most_common_label = max(set(neighbor_labels), key=neighbor_labels.count)
-
-            y_predicted.append(most_common_label)
-
-        return y_predicted
-    
-
-def mixed_distance(instance1, instance2, categorical_indices):
-    """Calculates the mixed distance between two instances.
-
-    Args:
-        instance1 (list): First instance.
-        instance2 (list): Second instance.
-        categorical_indices (list of int): List of indices for categorical features.
-
-    Returns:
-        float: The mixed distance between instance1 and instance2.
-    """
-    distance = 0
-    for i, (val1, val2) in enumerate(zip(instance1, instance2)):
-        if i in categorical_indices:
-            # Categorical: add 1 if values are different, 0 if they are the same
-            distance += 0 if val1 == val2 else 1
-        else:
-            # Numerical: use Euclidean distance component
-            distance += (val1 - val2) ** 2
-    return distance ** 0.5  # Take square root for Euclidean component
-
-class MyKNeighborsCatClassifier:
-    """Represents a simple k nearest neighbors classifier.
-
-    Attributes:
-        n_neighbors(int): number of k neighbors
-        X_train(list of list of numeric vals): The list of training instances (samples).
-                The shape of X_train is (n_train_samples, n_features)
-        y_train(list of obj): The target y values (parallel to X_train).
-            The shape of y_train is n_samples
-
-    Notes:
-        Loosely based on sklearn's KNeighborsClassifier:
-            https://scikit-learn.org/stable/modules/generated/sklearn.neighbors.KNeighborsClassifier.html
-        Terminology: instance = sample = row and attribute = feature = column
-        Assumes data has been properly normalized before use.
-    """
-    def __init__(self, n_neighbors=3, categorical_indices=None):
-        """Initializer for MyKNeighborsClassifier.
-
-        Args:
-            n_neighbors(int): number of k neighbors
-            categorical_indices(list of int, optional): List of indices of categorical features
-        """
-        self.n_neighbors = n_neighbors
-        self.categorical_indices = categorical_indices if categorical_indices is not None else []
-        self.X_train = None
-        self.y_train = None
-
-    def fit(self, X_train, y_train):
-        self.X_train = X_train
-        self.y_train = y_train
-
-    def kneighbors(self, X_test):
-        all_distances = []
-        all_neighbors = []
-
-        for test_instance in X_test:
-            dist = []
-            for i, train_instance in enumerate(self.X_train):
-                # Use mixed distance function
-                distance = mixed_distance(test_instance, train_instance, self.categorical_indices)
-                dist.append((distance, i))
-            dist.sort(key=lambda x: x[0])  # Sort based on distance
-            current_distances = [d[0] for d in dist[:self.n_neighbors]]
-            current_neighbors = [d[1] for d in dist[:self.n_neighbors]]
-            all_distances.append(current_distances)
-            all_neighbors.append(current_neighbors)
-
-        return all_distances, all_neighbors
-
-    def predict(self, X_test):
-        _, neighbor_indices = self.kneighbors(X_test)
-        y_predicted = []
-        for neighbors in neighbor_indices:
-            neighbor_labels = [self.y_train[i] for i in neighbors]
-            most_common_label = max(set(neighbor_labels), key=neighbor_labels.count)
-            y_predicted.append(most_common_label)
+        for indices in neighbor_indices:  # Predict based on majority vote
+            neighbor_classes = [self.y_train[i] for i in indices]  # Get classes of neighbors
+            predicted_class = max(set(neighbor_classes), key=neighbor_classes.count)  # Majority vote
+            y_predicted.append(predicted_class)  # Append predicted class
         return y_predicted
--- utils.py
+++ utils.py
@@ -3,7 +3,7 @@ import random
 import matplotlib.pyplot as plt
 import numpy as np
 from mypytable import MyPyTable
-from tabulate import tabulate
+
 
 
 def euclid_dist(pt1, pt2):
@@ -48,8 +48,6 @@ def train_test_split(X, y, test_size=0.33, random_state=None, shuffle=True):
         Loosely based on sklearn's train_test_split():
             https://scikit-learn.org/stable/modules/generated/sklearn.model_selection.train_test_split.html
     """
-    # Set a specific seed for random number generation if a random state is provided
-    """Split dataset into train and test sets based on a test set size."""
     # Set a specific seed for random number generation if a random state is provided
     if random_state is not None:
         np.random.seed(random_state)
@@ -111,13 +109,13 @@ def normalize_data(data):
 
 def rand_inds(num, total):
     """Creates a list of num random indices from 0 to total-1."""
-    random.seed(1)  
+    random.seed(1)
     inds = []
     for _ in range(num):  # Iterate num times
         inds.append(random.randint(0, total - 1))  # Adjusted to valid index range
     return inds
 
-def freq_plot(infile, col_name_x):
+def freq_plot(data, header,col_name_x):
     """Generates a frequency plot for the specified column in the data file.
 
     Args:
@@ -128,7 +126,7 @@ def freq_plot(infile, col_name_x):
         None
     """
     # Load the data table from the specified file
-    tbl = MyPyTable().load_from_file(infile)
+    tbl = MyPyTable(header, data)
     x_unsorted = tbl.get_column(col_name_x)
     x_list = sorted(x_unsorted)
     unique_vals = []  # To hold unique values
@@ -152,9 +150,132 @@ def freq_plot(infile, col_name_x):
 
     # Create a bar plot
     plt.figure()
-    plt.bar(x, y)
+    color1= generate_cute_color()
+    plt.bar(x, y, color=color1)
     # Label plot
     plt.xlabel(col_name_x)
     plt.ylabel('count')
     plt.title(f'Total Number of {col_name_x}')
     plt.show()
+
+
+
+def generate_cute_color():
+    """
+    Generates a random hex color within a "cute" range.
+
+    Returns:
+        str: A hex color string (e.g., "#FFB6C1").
+    """
+    # Define ranges for "cute" colors (lighter tones)
+    # Higher red, green, and blue values result in pastel-like colors
+    red = random.randint(180, 255)
+    green = random.randint(150, 255)
+    blue = random.randint(200, 255)
+
+    # Convert to hex
+    return f"#{red:02X}{green:02X}{blue:02X}"
+
+
+def calculate_accuracy_error_rate(results):
+    """Calculate the accuracy and error rate from the results of cross-validation.
+
+    Args:
+        results (list of tuples): Each tuple contains the true labels (y_test) and the predicted labels (y_pred).
+            Each tuple is of the form (y_test, y_pred).
+
+    Returns:
+        accuracy (float): The accuracy of the classifier.
+        error_rate (float): The error rate of the classifier.
+    """
+    correct = 0  # Initialize a counter for correct predictions
+    total = 0    # Initialize a counter for the total number of predictions
+
+    # Iterate through the results for each fold
+    for y_test, y_pred in results:
+        # Count the number of correct predictions for each fold
+        correct += sum([1 for true, pred in zip(y_test, y_pred) if true == pred])
+        total += len(y_test)  # Update the total number of predictions
+
+    # Calculate accuracy as the ratio of correct predictions to total predictions
+    accuracy = correct / total
+    # Calculate error rate as the complement of accuracy
+    error_rate = 1 - accuracy
+
+    return accuracy, error_rate
+
+def calculate_precision_recall_f1(results):
+    """Calculate precision, recall, and F1 score from the results of cross-validation.
+
+    Args:
+        results (list of tuples): Each tuple contains the true labels (y_test) and the predicted labels (y_pred).
+            Each tuple is of the form (y_test, y_pred).
+
+    Returns:
+        precision (float): The precision of the classifier.
+        recall (float): The recall of the classifier.
+        f1 (float): The F1 score of the classifier.
+    """
+    true_positive = 0  # Initialize counter for true positives
+    false_positive = 0  # Initialize counter for false positives
+    false_negative = 0  # Initialize counter for false negatives
+    true_negative = 0  # Initialize counter for true negatives
+
+    # Iterate through each fold's true and predicted values
+    for y_test, y_pred in results:
+        for true, pred in zip(y_test, y_pred):
+            # Count true positives, false positives, false negatives, and true negatives
+            if true == 1 and pred == 1:
+                true_positive += 1
+            elif true == 0 and pred == 1:
+                false_positive += 1
+            elif true == 1 and pred == 0:
+                false_negative += 1
+            elif true == 0 and pred == 0:
+                true_negative += 1
+
+    # Calculate precision (how many predicted positives are actually positive)
+    precision = true_positive / (true_positive + false_positive) if (true_positive + false_positive) > 0 else 0
+    # Calculate recall (how many actual positives were correctly identified)
+    recall = true_positive / (true_positive + false_negative) if (true_positive + false_negative) > 0 else 0
+    # Calculate F1 score (harmonic mean of precision and recall)
+    f1 = 2 * (precision * recall) / (precision + recall) if (precision + recall) > 0 else 0
+
+    return precision, recall, f1
+
+def calculate_confusion_matrix(results):
+    """Calculate the confusion matrix from the results of cross-validation.
+
+    Args:
+        results (list of tuples): Each tuple contains the true labels (y_test) and the predicted labels (y_pred).
+            Each tuple is of the form (y_test, y_pred).
+
+    Returns:
+        conf_matrix (list of list): A confusion matrix as a 2x2 list of counts.
+            The matrix will have the following format:
+            [[true_negative, false_positive],
+             [false_negative, true_positive]]
+    """
+    true_positive = 0  # Initialize counter for true positives
+    false_positive = 0  # Initialize counter for false positives
+    false_negative = 0  # Initialize counter for false negatives
+    true_negative = 0  # Initialize counter for true negatives
+
+    # Iterate through each fold's true and predicted values
+    for y_test, y_pred in results:
+        for true, pred in zip(y_test, y_pred):
+            # Count true positives, false positives, false negatives, and true negatives
+            if true == 1 and pred == 1:
+                true_positive += 1
+            elif true == 0 and pred == 1:
+                false_positive += 1
+            elif true == 1 and pred == 0:
+                false_negative += 1
+            elif true == 0 and pred == 0:
+                true_negative += 1
+
+    # Create confusion matrix in the format: [[TN, FP], [FN, TP]]
+    conf_matrix = [[true_negative, false_positive],
+                   [false_negative, true_positive]]
+
+    return conf_matrix
